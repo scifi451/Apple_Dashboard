1f34cecb3028935048483f8f2f1d41a78aaa8088
Matt Pollari <mdpollari@gmail.com>
2020-02-04 20:00:03 -0600

    iPad and wearables graphs  working.

--- ipad.js
+++ ipad.js
@@ -1,12 +1,8 @@
 // Create our first trace
 var trace1 = {
-    y: [23.95, 22.28, 15.82, 16.65, 30.66, 22.96, 18.15, 19.51, 32.5, 26.06, 19.75, 23.68, 51.18, 40.28, 31.37, 32.21, 51.64, 32.86, 24.05, 28.16, 54.38, 33.25, 24.85, 28.85, 61.58, 37.56, 29.47, 37.19, 51.98, 31.05, 25.99, 33.36],
+    y: [8.77, 6.26, 8.78, 7.13, 10.67, 8.75, 6.37, 6.19, 11.47, 7.61, 5.89, 5.32, 8.99, 5.43, 4.54, 4.28, 7.08, 4.41, 4.88, 4.26, 5.53, 3.89, 4.97, 4.83, 5.86, 4.01, 4.63, 4.09, 6.73, 4.87, 5.02, 4.66],
     x: ["1Q '12", "2Q '12", "3Q '12", "4Q '12", "1Q '13", "2Q '13", "3Q '13", "4Q '13", "1Q '14", "2Q '14", "3Q '14", "4Q '14", "1Q '15", "2Q '15", "3Q '15", "4Q '15", "1Q '16", "2Q '16", "3Q '16", "4Q '16", "1Q '17", "2Q '17", "3Q '17", "4Q '17", "1Q '18", "2Q '18", "3Q '18", "4Q '18", "1Q '19", "2Q '19", "3Q '19", "4Q '19"],
-    type: "scatter",
-    line: {
-        color: 'green',
-        width: 1.5
-      }
+    type: "scatter"
   };
   
 
@@ -15,7 +11,7 @@ var data = [trace1];
 
 // Apply the group barmode to the layout
 var layout = {
-  title: "iPhone Revenue by Quarter",
+  title: "iPad Revenue by Quarter",
   xaxis: { title: "Quarter" },
   yaxis: { title: "Revenue in Billons"}
 };
@@ -23,7 +19,7 @@ var layout = {
 
 // Note that we omitted the layout object this time
 // This will use default parameters for the layout
-Plotly.newPlot("plot2", data, layout);
+Plotly.newPlot("plot3", data, layout);
 
 
 
--- other.js
+++ other.js
@@ -1,12 +1,8 @@
 // Create our first trace
 var trace1 = {
-    y: [23.95, 22.28, 15.82, 16.65, 30.66, 22.96, 18.15, 19.51, 32.5, 26.06, 19.75, 23.68, 51.18, 40.28, 31.37, 32.21, 51.64, 32.86, 24.05, 28.16, 54.38, 33.25, 24.85, 28.85, 61.58, 37.56, 29.47, 37.19, 51.98, 31.05, 25.99, 33.36],
+    y: [4, 2.4, 2.29, 2.08, 3.97, 2.34, 1.91, 1.89, 2.84, 1.88, 1.77, 1.9, 2.69, 1.69, 2.64, 3.05, 4.35, 2.19, 2.22, 2.37, 4.02, 2.87, 2.74, 3.23, 5.49, 3.94, 3.73, 4.23, 7.31, 5.13, 5.53, 6.52],
     x: ["1Q '12", "2Q '12", "3Q '12", "4Q '12", "1Q '13", "2Q '13", "3Q '13", "4Q '13", "1Q '14", "2Q '14", "3Q '14", "4Q '14", "1Q '15", "2Q '15", "3Q '15", "4Q '15", "1Q '16", "2Q '16", "3Q '16", "4Q '16", "1Q '17", "2Q '17", "3Q '17", "4Q '17", "1Q '18", "2Q '18", "3Q '18", "4Q '18", "1Q '19", "2Q '19", "3Q '19", "4Q '19"],
-    type: "scatter",
-    line: {
-        color: 'green',
-        width: 1.5
-      }
+    type: "scatter"
   };
   
 
@@ -15,7 +11,7 @@ var data = [trace1];
 
 // Apply the group barmode to the layout
 var layout = {
-  title: "iPhone Revenue by Quarter",
+  title: "Wearables, home and accessories Revenue by Quarter",
   xaxis: { title: "Quarter" },
   yaxis: { title: "Revenue in Billons"}
 };
@@ -23,7 +19,7 @@ var layout = {
 
 // Note that we omitted the layout object this time
 // This will use default parameters for the layout
-Plotly.newPlot("plot2", data, layout);
+Plotly.newPlot("plot4", data, layout);
 
 
 
